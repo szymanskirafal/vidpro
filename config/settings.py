4318b29f477830df3d1244ecf08d14a094962f18
Rafal Szymanski <r.szymansky@gmail.com>
2021-03-16 22:36:54 +0100

    Set Django_debu default

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DJANGO_DEBUG')
+DEBUG = env.bool("DJANGO_DEBUG", default=True)
 
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=['.herokuapp.com', 'localhost', '127.0.0.1', '0.0.0.0', ])
 
