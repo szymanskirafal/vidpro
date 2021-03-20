108dcaa3ba317e213787b2acabf878d30c409d47
Rafal Szymanski <r.szymansky@gmail.com>
2021-03-20 08:51:01 +0100

    Add custom domain to ALLOWED HOST

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,16 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=True)
 
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=['.herokuapp.com', 'localhost', '127.0.0.1', '0.0.0.0', ])
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS",
+    default=[
+        'www.tryread.com',
+        '.herokuapp.com',
+        'localhost',
+        '127.0.0.1',
+        '0.0.0.0',
+        ]
+    )
 
 
 # Application definition
